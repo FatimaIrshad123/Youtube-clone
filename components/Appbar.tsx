822994ad382362eeb427340df5fe28f2e93a9ec0
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-22 11:42:01 +0500

    Added Youtube logo

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -3,7 +3,8 @@ import { Searchbar } from "./Searchbar";
 export function Appbar(){
     return (
         <div className="flex justify-between pt-1 p-2">
-            <div className="text-md inline-flex items-center pb-2">
+            <div className="text-md inline-flex items-center pb-2 font-bold text-2xl">
+                <img src="/ytb.png" className="h-6 w-8"></img>
                 Youtube
             </div>
             <div>
