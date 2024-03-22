cdeb47100696e7df522d330ffce802a2b62c15a4
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-22 09:52:49 +0500

    Updated VideoCard component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div>
       Hi  
-      <VideoCard />
+      <VideoCard 
+        title={'How to learn coding | 30 days plan | code with me'}
+        image={'/img.png'} 
+        author={'Fatima'} 
+        views={'10M'} 
+        timestamp={'2 weeks ago'} 
+        thumbnail={'/img1.png'}/>
+        
     </div>
   );
 }
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,21 +1,21 @@
 
-export function VideoCard(){
+export function VideoCard(props:any){
     return (
         <div>
-            <img className='rounded-xl'src="/img.png"/>
+            <img className='rounded-xl'src={props.image}/>
             <div className="grid grid-cols-12 pt-2">
-                <div className="col-span-1 pl-2">
-                    <img className= {'rounded-full w-12 h-12'}src="/img.png"/>
+                <div className="col-span-1 ">
+                    <img className= {'rounded-full w-12 h-12'}src={props.thumbnail}/>
                 </div>
                 <div className="col-span-11">
                     <div>
-                    Pokemon | Fatima
+                        {props.title}
+                    </div>
+                <div className="col-span-11 text-gray-600 text-base">
+                    {props.author}
                 </div>
                 <div className="col-span-11 text-gray-600 text-base">
-                    Fatima
-                </div>
-                <div className="col-span-11 text-gray-600 text-base">
-                   14M | 13 days ago
+                   {props.views} | {props.timestamp}
                 </div>
             </div>
             </div>
